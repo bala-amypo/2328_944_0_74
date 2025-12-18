8e92942d6392af973fab57bab5a2377d22023b51
bala-amypo <bala@amypo.in>
2025-12-18 05:48:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Studentservice.java
@@ -0,0 +1,39 @@
+package com.example.project.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.example.project.entity.Studententity;
+
+@Service
+public class Studentservice {
+    Map<Integer,com.example.project.entity.Studententity>mp=new HashMap<>();
+       
+  public Studententity savedata(Studententity st){
+    mp.put(st.getId(), st);
+    return st;
+  }
+
+  public List<Studententity> retdata() {
+    return new ArrayList<>(mp.values());
+  }
+
+  public Studententity id(int id) {
+   return mp.get(id);
+  }
+
+  public Studententity update(int id, Studententity st) {
+  mp.replace(id, st);
+  return st;
+  }
+
+  public Studententity deletes(int id, Studententity st){
+     mp.remove(id,st);
+    return st;
+  }
+    
+}
